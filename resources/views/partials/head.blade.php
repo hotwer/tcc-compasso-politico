7f155002e3c6af126a96cce06c3ad1e779cd7be7
Bernardo Araujo <bernardo.k.araujo@gmail.com>
2021-12-03 07:50:14 -0300

    adicionado versionamento manual para o load dos arquivos js e css

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -5,9 +5,9 @@
 <link rel="icon" type="x-icon" href="icon.png">
 <link rel="shortcut icon" type="x-icon" href="icon.png">
 <link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,700|Roboto:400,700" rel="stylesheet">
-<link href='style.css' rel='stylesheet' type='text/css'>
+<link href='style.css?v=1' rel='stylesheet' type='text/css'>
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/0.24.0/axios.min.js"
 integrity="sha512-u9akINsQsAkG9xjc1cnGF4zw5TFDwkxuc9vUp5dltDWYCSmyd0meygbvgXrlc/z7/o4a19Fb5V0OUE58J7dcyw=="
 crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-<script src="/lib.js"></script>
+<script src="/lib.js?v=1"></script>
